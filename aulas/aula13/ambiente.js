f1bd20c20e4b766bbb75d4cb006daf21830183a3
AlePetrone21 <91491923+AlePetrone21@users.noreply.github.com>
2022-04-16 18:00:43 -0300

    FInalização da Aula13 e 14 P1 e P2

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,27 @@
+//  REPETIÇÃO NO FINAL
+var c = 1
+do {
+    console.log(`${c}º Tudo bem?`);
+    c++
+} while (c <=6)
+
+
+
+
+/*
+    REPETIÇÃO NO INÍCiO
+var c = 1
+while (c <= 6) {
+    console.log('${c}º Tudo bem?');
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
